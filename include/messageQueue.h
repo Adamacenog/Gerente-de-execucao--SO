b91e1de5a992044bd094d8ff1d40b3f0aeeda3fb
alexnsouza6 <alexnsouza6@hotmail.com>
2019-05-20 09:58:35 -0300

    adds include for queue libraries

--- include/messageQueue.h
+++ include/messageQueue.h
@@ -5,6 +5,12 @@ Andre Garrido Damaceno.- mat. 15/0117531
 Danillo Neves. - mat. 14/0135839
 Guilherme Lopes
 */
+#ifndef _Queue_Definition
+  #define _Queue_Definition
+    #include <sys/types.h>
+    #include <sys/ipc.h>
+    #include <sys/msg.h>
+#endif
 
 #define MSGSZ 500
 
@@ -16,7 +22,7 @@ typedef struct msgbuf
 } msgbuf;
 
 int QueueCreator(key_t);
-void MessageReceive(int, struct msgbuf *, long);
+void MessageReceive(int, struct msgbuf *, long, int);
 void MessageSend(int, struct msgbuf, size_t);
 void QueueDestroy(int msqid);
 void CreateMessage(int, int, char *, char *, long);
